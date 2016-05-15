bdc7fee531a94ecfae8e190458040e547943cc73
wangmengling <294025529@qq.com>
2016-05-15 22:54:32 +0800

    asdf

--- demo/CarouselView.swift
+++ demo/CarouselView.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
+let timeInterval:NSTimeInterval = 2
+
 class CarouselView: UIView, UIScrollViewDelegate{
-//    var imageArray:Array {
-//        didSet  {
-//            
-//        }
-//    }
+    
+    var imageArray:Array! {
+        willSet(newValue) {
+            self.imageArray = newValue
+        }
+        didSet  {
+            
+        }
+    }
     
     var contentScrollView: UIScrollView!
     
@@ -41,10 +47,40 @@ class CarouselView: UIView, UIScrollViewDelegate{
     
     convenience init(frame: CGRect, imageArray: [UIImage!]?) {
         self.init(frame: frame)
+        self.imageArray = imageArray
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func buildView(frame:CGRect) -> Void {
+        
+        
+        
+        contentScrollView = UIScrollView(frame: frame)
+        contentScrollView.pagingEnabled = true
+        self.addSubview(contentScrollView)
+        
+        
+        pageControl = UIPageControl(frame: frame)
+        self.addSubview(pageControl)
+        
+        
+        currentImageView = UIImageView()
+        self.addSubview(currentImageView)
+        preImageView = UIImageView()
+        self.addSubview(preImageView)
+        nextImageView = UIImageView()
+        self.addSubview(nextImageView)
+        
+        
+        timer = NSTimer(timeInterval: timeInterval, target: self, selector: #selector(CarouselView.carouseAction), userInfo: nil, repeats: true)
+    }
+    
+    func carouseAction() -> Void {
+        self.currentPageIndex = self.currentPageIndex + 1
+        self.contentScrollView.contentOffset = CGPoint(x: CGFloat(self.currentPageIndex)  * self.frame.size.width, y: 0)
     }
     
     
